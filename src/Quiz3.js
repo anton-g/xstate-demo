b3dc422741d6855a9f00f9043239eaee7ff82c4d
Anton Gunnarsson <gson.anton@hotmail.com>
2022-06-13 20:04:37 +0200

    Actual done

--- src/Quiz3.js
+++ src/Quiz3.js
@@ -1,14 +1,52 @@
+import { useMachine } from '@xstate/react'
+import { quizMachine } from './quizMachine'
+
 export function Quiz3() {
+  const [state, send] = useMachine(quizMachine)
+
   return (
     <main>
       <h1>Game show buzzer 3</h1>
-      <div>
-        <button>start</button>
-        <button>buzz</button>
-        <button>incorrect</button>
-        <button>correct</button>
+      <div style={{ marginTop: 16 }}>
+        <button
+          disabled={!state.can('START')}
+          onClick={() => {
+            send('START')
+          }}
+        >
+          start
+        </button>
+        <button
+          disabled={!state.can('BUZZ')}
+          onClick={() => {
+            send('BUZZ')
+          }}
+        >
+          buzz
+        </button>
+        <button
+          disabled={!state.can('INCORRECT')}
+          onClick={() => {
+            send('INCORRECT')
+          }}
+        >
+          incorrect
+        </button>
+        <button
+          disabled={!state.can('CORRECT')}
+          onClick={() => {
+            send('CORRECT')
+          }}
+        >
+          correct
+        </button>
       </div>
-      <p>state</p>
+      <p>
+        {state.matches('intro') && 'intro'}
+        {state.matches('idle') && 'idle'}
+        {state.matches('buzzed') && 'buzzed!'}
+        {state.matches('ended') && 'ended'}
+      </p>
     </main>
   )
 }
